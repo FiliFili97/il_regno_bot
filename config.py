a367df70310df5619054a3a59f1ec93d5946aaa0
FiliFili97 <pippo9760@gmail.com>
2025-06-18 18:43:33 +0200

    Update config.py

--- config.py
+++ config.py
@@ -2,4 +2,4 @@ import os
 
 TOKEN = os.getenv("TOKEN")
 DEEPSEEK_API_KEY = os.getenv("DEEPSEEK_API_KEY")
-GROUP_CHAT_ID = -4870133880  # questo può restare hardcoded
+GROUP_CHAT_ID = -4870133880  # GROUP CHAT ID
