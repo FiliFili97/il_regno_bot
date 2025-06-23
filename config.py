361a90f2e69fe0480dfc85bc891fc1d307c34a3e
FiliFili97 <pippo9760@gmail.com>
2025-06-23 17:51:52 +0200

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ load_dotenv()
 
 # Telegram
 TOKEN = os.getenv("TOKEN")
-GROUP_CHAT_ID = int(os.getenv("GROUP_CHAT_ID", "-4870133880"))
+GROUP_CHAT_ID = None
 
 # Gemini (Gemma 3)
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
